b0e34ad033ebc5417883ef313243e459c4b320c0
Erlang P <erlangparasu>
2021-02-24 10:24:20 +0800

    cleanup

---
+++ home.php
@@ -0,0 +1,7 @@
+<?php
+
+require 'core.php';
+
+// _generate('password', '<?php echo strtoupper("belajar enkripsi") . "\n"; ');
+
+_run('MoFkkD84Ss813hSrbUXPkmfc8ShUDlpwvKxieI4l%2BMgaqOGV7U6Bk5voCoA%2Fo66PEWrQqlscwD0lyIhVIYQMDfQScZVTuNHU6j3%2B931iTDSajIYXyNmsZEGnbO71YO3Q70Zt7HG2K10Zy1y6UTx7Vw%3D%3D');
---
+++ info.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'core.php';
+
+_run('az%2BG49hi6KLQ%2FEb1H3X%2FScjN15yeaGGhzTM%2FloUDrZows7Eb4P9NNVm6zT9%2FyT7zeqsJuY%2FQOZpiZUaD%2B7huySvfG6V5VZ3VUsWF8W%2Ff08ZBkHq%2B3Okbe9O1iy8kYlWD');
